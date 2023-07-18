164d3d56679b94fcfaf992bc7f700da2391f285d
Philipp Heuer <git@philippheuer.me>
2023-07-18 21:59:38 +0200

    feat: explicit content-type

--- src/main/java/io/github/primelib/perspective4j/api/PerspectiveApi.java
+++ src/main/java/io/github/primelib/perspective4j/api/PerspectiveApi.java
@@ -6,6 +6,7 @@ import javax.annotation.processing.Generated;
 import io.github.primelib.perspective4j.model.CommentAnalyzeRequest;
 import io.github.primelib.perspective4j.model.CommentAnalyzeResult;
 import feign.RequestLine;
+import feign.Headers;
 
 @Generated(value = "io.github.primelib.primecodegen.javafeign.JavaFeignGenerator")
 public interface PerspectiveApi {
@@ -16,6 +17,9 @@ public interface PerspectiveApi {
      * @param commentAnalyzeRequest  (required)
      */
     @RequestLine("POST /v1alpha1/comments:analyze")
-    CommentAnalyzeResult analyzeCommentV1Alpha1( @NotNull CommentAnalyzeRequest commentAnalyzeRequest);
+    @Headers({
+        "Content-Type: application/json"
+    })
+    CommentAnalyzeResult analyzeCommentV1Alpha1(@NotNull CommentAnalyzeRequest commentAnalyzeRequest);
 
 }
